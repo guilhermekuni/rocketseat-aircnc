da232e8c3853f479f87a372494420ce43eafc097
unknown <guilhermekuni@gmail.com>
2019-10-12 15:53:17 -0300

    Adjusts backend and create web login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
 
+import api from './services/api';
+
+import logo from './assets/logo.svg';
+
 function App() {
+  const [email, setEmail] = useState('');
+
+  async function handleSubmit(event) {
+    event.preventDefault();
+    
+    const response = await api.post('/sessions', { email });
+    console.log(response);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <img src={logo} alt="AirCnc" />
+
+      <div className="content">
+        <p>Ofereça <strong>spots</strong> para programadoress e encontre <strong>talentos</strong> para sua empresa.</p>
+
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email">E-MAIL *</label>
+          <input
+            type="text"
+            id="email"
+            placeholder="Seu melhor e-mail"
+            value={email}
+            onChange={event => setEmail(event.target.value)}
+          />
+
+          <button className="btn" type="submit">Entrar</button>
+        </form>
+      </div>
     </div>
   );
 }
 
-export default App;
+export default App; 
