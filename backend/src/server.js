1295d5ce77a0e4a825dced3f68fc4942c1e06285
unknown <guilhermekuni@gmail.com>
2019-10-12 17:02:14 -0300

    Implements dashboard screen

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path');
 
 const routes = require('./routes');
 
@@ -13,6 +14,7 @@ mongoose.connect('mongodb+srv://guilhermekuni:guikuni20@aircnc-5wq3w.mongodb.net
 
 app.use(cors());
 app.use(express.json());
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads')));
 app.use(routes);
 
 app.listen(3333);
